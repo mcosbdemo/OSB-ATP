0b1b64bf62edeeceb941064b9f318f196e93cca2
Kwan Wan <kwan.wan@oracle.com>
2019-10-06 17:01:43 +0800

    Update store.js

--- src/js/viewModels/store.js
+++ src/js/viewModels/store.js
@@ -21,26 +21,42 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojtabs', 'ojs
        */
       self.connected = function() {
         // Implement if needed
-        var map = new google.maps.Map(document.getElementById('googleMap'), {
-            zoom: 18,
-            center: {lat: 22.2797636, lng: 114.1834475}
-        });
-        $.getJSON("data/stores.json",
-                function (data)
-                {
+        // kenneth: replace map with
+        // var map = new google.maps.Map(document.getElementById('googleMap'), {
+        //    zoom: 18,
+        //    center: {lat: 22.2797636, lng: 114.1834475}
+        // });
+        // kenneth: map definition
+        var map = L.map(document.getElementById('googleMap')).setView([22.2797636,114.1834475],18);
+        // $.getJSON("data/stores.json",
+        //        function (data)
+        //        {
                     //var label = 'Cafe Supremo';
-                    console.log("Loaded data: " + data);
-                    self.stores(new oj.ArrayTableDataSource(data, {idAttribute: "id"}));
-                    var markers = data.map(function (location, i) {
-                        return new google.maps.Marker({
-                            position: location,
-                            label: "Cafe Supremo"
-                        });
-                    });
-                    var markerCluster = new MarkerClusterer(map, markers,
-                            {imagePath: 'img/cup-of-coffee-icon'});
+        //            console.log("Loaded data: " + data);
+        //            self.stores(new oj.ArrayTableDataSource(data, {idAttribute: "id"}));
+        //            var markers = data.map(function (location, i) {
+        //                return new google.maps.Marker({
+        //                    position: location,
+        //                    label: "Cafe Supremo"
+        //                });
+        //            });
+        //            var markerCluster = new MarkerClusterer(map, markers,
+        //                    {imagePath: 'img/cup-of-coffee-icon'});
 
-                })
+        //        })
+        // kenneth - this section define the 6 stores
+        var markerA = L.marker([22.2797636,114.1834475]).addTo(map);
+        var markerB = L.marker([22.2794423,114.182937]).addTo(map);
+        var markerC = L.marker([22.28037511,114.185295]).addTo(map);
+        var markerD = L.marker([22.2799102,114.1841145]).addTo(map);
+        var markerE = L.marker([22.280582,114.1821902]).addTo(map);
+        var markerF = L.marker([22.2805063,114.1822464]).addTo(map);
+        // kenneth - this actually add the map
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            { attribution:
+               'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+              maxZoom:18}
+          ).addTo(map);
       };
 
       /**
